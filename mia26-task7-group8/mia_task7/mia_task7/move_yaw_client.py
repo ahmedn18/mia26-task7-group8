7c136aeb03702c8e5e37a0232b5dfea6cee20ecb
morii <masamostafa2017@gmail.com>
2026-08-19 18:29:23 +0300

    fixed some bugs and renamed the action files to merge with my team mate's server code

--- mia26-task7-group8/mia_task7/mia_task7/move_yaw_client.py
+++ mia26-task7-group8/mia_task7/mia_task7/move_yaw_client.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy import Node
-from turtleBot_interface.action import move
-from geometery_msgs.msg import Twist
-from nav_msgs.msg import Odometry
+from robot_action.action import Move
 
 class Action_client(Node):
     def __init__(self):
@@ -11,48 +9,17 @@ class Action_client(Node):
         super().__init__('move_client')
         self.action_client = rclpy.action.ActionClient(
             self, 
-            move, 
-            '/move
-            '
-        )
-
-        """#publishing to cmd_vel 
-        self.publisher_ = self.create_publisher(
-            Twist ,
-            '/cmd_vel',
-            10
-        ) 
-
-        #subscribe to odom
-        self.subscription = self.create_subscription(
-            Odometry,
-            '/odom',
-            self.odom_callback,
-            10 
-        )
-    #callback function to save odom data ( now i have the odom data in the callback function, but i need to save it to use it later)
-"""
-   """ def odom_callback(self, msg):
-        pos = msg.pose.pose.position
+            Move, 
+            '/move_yaw')
         
-        ori = msg.pose.pose.orientation
-        
-        linear_vel = msg.twist.twist.linear
-
-            f"Pos: X={pos.x:.2f}, Y={pos.y:.2f} | "
-            f"Ori: W={ori.w:.2f} | "
-            f"Speed: X={linear_vel.x:.2f} m/s"
-        self.get_logger().info(f"Pos: X={pos.x:.2f}, Y={pos.y:.2f} | Ori: W={ori.w:.2f} | Speed: X={linear_vel.x:.2f} m/s") #publishing the postion in terminal
-
-        )"""
     def send_goal(self):
         self.get_logger().info('Waiting for action server...')
         self.action_client.wait_for_server(timeout_sec=10.0)
-        goal_msg = move_yaw.Goal()
+        goal_msg = Move.Goal()
 
         #our goal is  
-        goal_msg.firstYaw = 90.0
-        goal_msg.secondYaw = 0.0
+        goal_msg.first_yaw = first_yaw
+        goal_msg.second_yaw = second_yaw
 
         self.send_goal_future = self.action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
         self.send_goal_future.add_done_callback(self.goal_response_callback)
@@ -67,8 +34,13 @@ class Action_client(Node):
 
     def feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
-        self.get_logger().info(f'Feedback received: {feedback.current_action}, Progress: {feedback.progress}%')
+            self.get_logger().info(f'Feedback received: {feedback.current_action}, Progress: {feedback.progress}%')
     def get_result_callback(self, future):
         result = future.result().result
         self.get_logger().info(f'Result received: Success: {result.success}, Message: {result.message}')
         rclpy.shutdown()
+def main():
+    rclpy.init()
+    node = MoveYawClient()
+    node.send_goal(first_yaw=90.0, second_yaw=0.0)
+    rclpy.spin(node)
