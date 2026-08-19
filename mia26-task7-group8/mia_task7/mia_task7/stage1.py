0320d8e537a8523d54fd987d1b22e37b9645b636
Dina Zaghloul <dina.gouda07@gmail.com>
2026-08-19 19:27:22 +0300

    Added Stage1 class

---
+++ mia26-task7-group8/mia_task7/mia_task7/stage1.py
@@ -0,0 +1,35 @@
+import asyncio
+import math
+
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from std_srvs.srv import SetBool
+
+from maze_nav_interfaces.action import MoveX, MoveYaw
+
+
+GATE_OPEN_S = 2.0
+
+
+class Stage1:
+
+    def __init__(self, node: Node, gate_client):
+
+        self.node = node
+        self.gate_client = gate_client
+
+        # Action servers
+        self.move_yaw_client = ActionClient(
+            node,
+            MoveYaw,
+            'move_yaw'
+        )
+
+        self.move_x_client = ActionClient(
+            node,
+            MoveX,
+            'move_x'
+        )
+
+    async def run(self) -> bool:
+        
